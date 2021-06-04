eb3c06cfd69a55155c7f2690a560abd40ad9bdde
Abhinav Sancheti <001sancheti@gmail.com>
2021-06-04 19:27:46 +0530

    added raycaster

--- my-portfolio/main.js
+++ my-portfolio/main.js
@@ -1,8 +1,6 @@
 import * as THREE from 'https://cdn.skypack.dev/three@0.129.0';
 import {OrbitControls} from 'https://cdn.skypack.dev/three@0.129.0/examples/jsm/controls/OrbitControls.js';
 import * as dat from 'dat.gui';
-console.log(dat);
-console.log(OrbitControls)
 
 const gui = new dat.GUI();
 const world = {
@@ -34,6 +32,7 @@ function generatePlane() {
     }
 }
 
+const raycaster = new THREE.Raycaster();
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(
     75,
@@ -52,13 +51,12 @@ document.body.appendChild(renderer.domElement);
 
 const planeGeometry = new THREE.PlaneGeometry(10, 10, 10, 10);
 const planeMaterial = new THREE.MeshPhongMaterial({
-    color: 0xffff00,
     side: THREE.DoubleSide,
     flatShading: THREE.FlatShading,
+    vertexColors: true
 });
 const planeMesh = new THREE.Mesh(planeGeometry, planeMaterial);
 scene.add(planeMesh);
-console.log(planeMesh.geometry.attributes.position.array);
 const { array } = planeMesh.geometry.attributes.position;
 for (let i = 0; i < array.length; i += 3) {
     const x = array[i];
@@ -66,6 +64,15 @@ for (let i = 0; i < array.length; i += 3) {
     const z = array[i + 2];
     array[i + 2] = z + Math.random();
 }
+
+const colors = []
+for (let i = 0; i < planeMesh.geometry.attributes.position.count; i++) {
+  colors.push(0, 1, 0)
+}
+console.log(colors);
+planeMesh.geometry.setAttribute('color', 
+new THREE.BufferAttribute(new 
+  Float32Array(colors), 3))
 
 //light
 const light = new THREE.DirectionalLight(0xfffff, 1);
@@ -77,9 +84,28 @@ const backLight = new THREE.DirectionalLight(0xfffff, 1);
 backLight.position.set(0, 0, -1);
 scene.add(backLight);
 
+const mouse = {
+  x: undefined,
+  y: undefined
+}
+
 function animate() {
     requestAnimationFrame(animate);
     renderer.render(scene, camera);
+    raycaster.setFromCamera(mouse, camera);
+    const intersects = raycaster.intersectObject(planeMesh);
+    if (intersects.length > 0) {
+      intersects[0].object.geometry.attributes.color.setX(0, 1)
+      intersects[0].object.geometry.attributes.color.needsUpdate = true
+    }
 }
 
 animate();
+
+
+//listeners 
+
+addEventListener('mousemove', (event) => {
+  mouse.x = (event.clientX / innerWidth) * 2 -1
+  mouse.y = -(event.clientY / innerHeight) * 2 +1
+})
